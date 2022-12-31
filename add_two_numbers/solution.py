164c32b2ffb7e183ac8755b2b1ad4d96cea9f1c5
brianxk <brianxk@proton.me>
2022-12-30 19:14:45 -0500

    Completed Python implementation of Add Two Numbers

--- add_two_numbers/solution.py
+++ add_two_numbers/solution.py
@@ -11,7 +11,9 @@ class Solution:
 
         carry = 0
 
-        while l1_ and l2_:
+        # Traverse both lists until one is depleted
+        # l1 will be the result list
+        while True:
             sum_ = l1_.val + l2_.val + carry
 
             if sum_ > 9:
@@ -22,8 +24,33 @@ class Solution:
 
             l1_.val = sum_
 
-            l1_ = l1.next
-            l2_ = l2.next
+            if not l1_.next or not l2_.next:
+                break
 
-
+            l1_ = l1_.next
+            l2_ = l2_.next
     
+        # If l1 was depleted first, append the rest of l2 to l1
+        # If l2 was depleted first or if both were depleted, skip this step
+        if l2_.next:
+            l1_.next = l2_.next
+
+        # If any digits remain, traverse and add until no carry remains
+        # If both lists were depleted, skip this step
+        while l1_.next and carry:
+            l1_ = l1_.next
+            sum_ = l1_.val + carry
+
+            if sum_ > 9:
+                sum_ -= 10
+                carry = 1
+            else:
+                carry = 0
+
+            l1_.val = sum_
+
+        # If both lists were depleted and a carry remains, add it to the most significant position
+        if carry:
+            l1_.next = ListNode(carry)
+
+        return l1
