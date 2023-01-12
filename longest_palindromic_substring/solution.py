3a3e04442a309dcc11c37111b483693dcda559db
brianxk <brianxk@proton.me>
2023-01-12 15:32:12 -0500

    longest palindrome substrings - brute force solution working

--- longest_palindromic_substring/solution.py
+++ longest_palindromic_substring/solution.py
@@ -2,8 +2,6 @@
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # Potential base case of len(s) == 1
-
         seen_chars = {}
         intervals = {}
 
@@ -12,12 +10,13 @@ class Solution:
                 targets = seen_chars[c]
 
                 for t in targets:
-                    interval = Interval(t, i)
+                    interval = (t, i)
+                    interval_len = i - t
 
-                    if interval.length() in intervals:
-                        intervals[interval.length()].append(interval)
+                    if interval_len in intervals:
+                        intervals[interval_len].add(interval)
                     else:
-                        intervals[interval.length()] = [interval]
+                        intervals[interval_len] = {interval}
 
                 seen_chars[c].append(i)
 
@@ -25,27 +24,41 @@ class Solution:
                 seen_chars[c] = [i]
 
         # Sort by interval length
-        interval = dict(sorted(intervals.items()))
+        intervals = dict(sorted(intervals.items()))
 
-        for interval_len, interval_list in intervals.items():
-            print(interval_len, interval_list)
+        palindrome_len = 1
+        palindrome_start = 0
+        palindrome_end = 1
 
-        return s
+        # Explore even palindromes 
+        base_intervals = (1, 2)
+        
+        for base_interval in base_intervals:
+            if base_interval in intervals:
+                for interval in intervals[base_interval]:
+                    interval_len = base_interval
 
-class Interval:
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
-        self.len = self.end - self.start
+                    interval_start = interval[0]
+                    interval_end = interval[1]
 
-    def length(self):
-        return self.len
+                    target_interval = (interval_start - 1, interval_end + 1)
+                    target_interval_len = interval_len + 2
 
-    def __str__(self):
-        return "{}:{}".format(self.start, self.end)
+                    while target_interval_len in intervals and target_interval in intervals[target_interval_len]:
+                        interval_len = target_interval_len
 
-    def __repr__(self):
-        return self.__str__()
+                        interval_start = target_interval[0]
+                        interval_end = target_interval[1]
+
+                        target_interval = (interval_start - 1, interval_end + 1)
+                        target_interval_len = interval_len + 2
+
+                    if interval_len + 1 > palindrome_len:
+                        palindrome_len = interval_len + 1
+                        palindrome_start = interval_start
+                        palindrome_end = interval_end + 1
+
+        return s[palindrome_start:palindrome_end]
 
 ### TESTING CODE ###
 
@@ -83,7 +96,7 @@ if __name__ == "__main__":
         test_cases = [ "abcddd", "abcdddd", "babad", "abcddc", "abcdddc", "abcddddc",
                        "ababababa", "n", "nn", "nnn", "nnnn", "nnnnn", "nnnnnnnnnnnn"
                        "babaababd", "cbbd", "abccba", "abcba", "abcddcba", "abcdefedcba"
-                       "abcdefgfedcba", "abcdefghhgfedcba" ]
+                       "abcdefgfedcba", "abcdefghhgfedcba", "aabbaabbaabbaa" ]
 
         # Generate randomized sequence of lowercase a-z and digits
         chars = list(string.ascii_lowercase + string.digits)
@@ -96,5 +109,5 @@ if __name__ == "__main__":
             print("Beginning tests for trial {}:".format(i + 1))
             for word in test_cases:
                 word = randomize_string(word, chars)
-                print(s.longestPalindrome(word))
+                print("s: {} p: {}".format(word, s.longestPalindrome(word)))
 
